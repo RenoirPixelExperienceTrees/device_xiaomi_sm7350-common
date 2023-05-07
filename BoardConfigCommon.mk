4657b3305524f93da3cde4f24b06bc5e4e934b0d
EndCredits <endcredits@crepuscular-aosp.icu>
2023-05-07 23:04:10 +0800

    sm7350-common: Initial framework compatibility matrix
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,9 +77,10 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    $(COMMON_PATH)/hidl/framework_compatibility_matrix.xml \
+    vendor/derp/config/device_framework_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
